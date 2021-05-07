07aa7e1509c9c01355f81523219e428cb0486756
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-07 13:41:45 -0700

    Add menu items and specials to Program, add color

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,15 +11,19 @@ namespace CounterProgram
       Console.WriteLine("Hello! Welcome to Pierre's Bakery.");
       Console.WriteLine("Today's menu prices are:");
       Console.WriteLine("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
-      Console.WriteLine("                                ");
+      Console.WriteLine();
       Console.ForegroundColor = ConsoleColor.Yellow;
-      Console.WriteLine("Loaf of bread - $5.00 each");
+      Console.WriteLine("Bread  - $5.00 per loaf");
       Console.WriteLine("Pastry - $2.00 each");
       Console.WriteLine("                                ");
-      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.ForegroundColor = ConsoleColor.Red;
       Console.WriteLine("We are currently running these specials:");
       Console.WriteLine("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
-      Console.WriteLine("                                ");
+      Console.WriteLine();
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.WriteLine("Bread: Buy 2 loaves, get 1 free!");
+      Console.WriteLine("Pastries: 3 for $5!");
+
 
 
 
