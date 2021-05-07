b8ba129cdb66bd61955aa5d92c976c41c76e304c
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-07 13:16:47 -0700

    Begin work on styling, add colors to text

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,23 @@ namespace CounterProgram
   {
     static void Main(string[] args)
     {
-      Console.WriteLine();
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine("Hello! Welcome to Pierre's Bakery.");
+      Console.WriteLine("Today's menu prices are:");
+      Console.WriteLine("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
+      Console.WriteLine("                                ");
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.WriteLine("Loaf of bread - $5.00 each");
+      Console.WriteLine("Pastry - $2.00 each");
+      Console.WriteLine("                                ");
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.WriteLine("We are currently running these specials:");
+      Console.WriteLine("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
+      Console.WriteLine("                                ");
+
+
+
+
 
     }
   }
